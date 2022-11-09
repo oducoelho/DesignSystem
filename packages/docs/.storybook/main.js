59bda98e9ecdd144f2201c50f42e2c56ca117d41
Eduardo Coelho <eduardocoelho619@gmail.com>
2022-11-08 21:39:11 -0300

    fix: add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,7 +16,7 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) =: {
+  viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/DesignSystem'
     }
